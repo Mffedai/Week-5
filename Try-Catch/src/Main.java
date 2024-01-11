d630a7e3c8e6bd3c7b6b548c804a0c6a2eff52b9
Mffedai <mfatihfedai@gmail.com>
2024-01-11 19:15:31 +0300

    Comment added

--- Try-Catch/src/Main.java
+++ Try-Catch/src/Main.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] list = new int[10];
-        for (int i = 0; i < 10; i++) {
+        int[] list = new int[10]; //Enter bound of list
+        for (int i = 0; i < 10; i++) {//Input element of list
             list[i] = i*i;
         }
         System.out.print("Please select a element of the list : ");
-
         try {
-            int element = input.nextInt();
+            int element = input.nextInt(); //User enters the value
             System.out.println(list[element]);
         } catch (Exception e) {
             System.out.println(e.getMessage());
